7749561a532ce966c44a0da68985511fde78789e
MarianBaba <marianbaba16@gmail.com>
2022-06-17 22:37:17 +0200

    book a seat

--- src/main/java/cinema/Cinema.java
+++ src/main/java/cinema/Cinema.java
@@ -45,27 +45,67 @@ public class Cinema {
 
 //        kConsecutives();
 
+        System.out.println("Enter the number of rows:");
+        int rows = scanner.nextInt();
+        System.out.println("Enter the number of seats in each row::");
+        int seats = scanner.nextInt();
+
         System.out.println("Cinema:");
 
         System.out.print("  ");
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < seats; i++) {
             System.out.print(i + 1 + " ");
         }
         System.out.println();
 
-        for (int i = 0; i < 7; i++) {
+        for (int i = 0; i < rows; i++) {
             System.out.print(i + 1 + " ");
-            for (int j = 0; j < 8; j++) {
+            for (int j = 0; j < seats; j++) {
                 System.out.print("S ");
             }
             System.out.println();
         }
 
-        System.out.println("Enter the number of rows: ");
-        int rows = scanner.nextInt();
-        System.out.println("Enter the number of seats in each row: ");
-        int seats = scanner.nextInt();
+        System.out.println();
 
+        System.out.println("Enter a row number:");
+        int desiredRow = scanner.nextInt();
+        System.out.println("Enter a seat number in that row:");
+        int desiredColumn = scanner.nextInt();
+
+        if (rows * seats <= 60) {
+            System.out.println("Ticket price: $10");
+        } else {
+            if (desiredRow <= rows/2) {
+                System.out.println("Ticket price: $10");
+            } else {
+                System.out.println("Ticket price: $8");
+            }
+        }
+
+        System.out.println("Cinema:");
+
+        System.out.print("  ");
+        for (int i = 0; i < seats; i++) {
+            System.out.print(i + 1 + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < rows; i++) {
+            System.out.print(i + 1 + " ");
+            for (int j = 0; j < seats; j++) {
+                if (i == desiredRow - 1 && j == desiredColumn - 1) {
+                    System.out.print("B ");
+                } else {
+                    System.out.print("S ");
+                }
+            }
+            System.out.println();
+        }
+
+
+
+        /*
         System.out.println("Total income:");
         if(rows * seats <= 60) {
             System.out.println("$" + (rows * seats * 10));
@@ -73,6 +113,6 @@ public class Cinema {
             int frontRows = rows / 2;
             System.out.println("$" + ((frontRows * seats * 10) + ((rows - frontRows) * seats * 8)));
         }
-
+        */
     }
 }
